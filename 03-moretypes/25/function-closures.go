0df20ba33a8dbb8c9be206c7737020b81cf1474a
yokoyamada <56547256+yokoyamada@users.noreply.github.com>
2020-11-12 09:47:08 +0900

    add my own understanding of the closure.

--- 03-moretypes/25/function-closures.go
+++ 03-moretypes/25/function-closures.go
@@ -1,3 +1,7 @@
+// closure とは「外部の変数を参照する関数値」と定義される。
+// この例でいうと adder() の中の func(x int) int が closure。
+// この closure は adder() スコープの変数 sum にアクセスすることができる。
+// オブジェクト指向で言うところのインスタンス内の変数にアクセスする振る舞い（関数）といった感じ？
 package main
 
 import (
@@ -14,6 +18,8 @@ func adder() func(int) int {
 
 func main() {
 
+	// pos と neg には別々の adder() が割り当てられている。
+	// 当然、それぞれの中の変数 sum は互いに独立している。
 	pos, neg := adder(), adder()
 	for i := 0; i < 10; i++ {
 		fmt.Println(
